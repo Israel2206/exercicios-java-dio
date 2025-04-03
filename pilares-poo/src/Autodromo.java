8c02e16ce173937eefe1f04ffb87a266a6fbf0d2
Israel <brenoisrael931@gmail.com>
2025-04-03 14:00:29 -0300

    POO

---
+++ pilares-poo/src/Autodromo.java
@@ -0,0 +1,13 @@
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro jeep = new Carro();
+
+        jeep.setChassi("6256633");
+        jeep.ligar();
+
+        Moto s1000rr = new Moto();
+        s1000rr.setChassi("03082007");
+        
+    }
+    
+}
---
+++ pilares-poo/src/Veiculo.java
@@ -0,0 +1,10 @@
+public class Veiculo {
+    private String Chassi;
+
+    public String getChassi() {
+        return Chassi;
+    }
+    public void setChassi(String chassi) {
+        Chassi = chassi;
+    }
+}
